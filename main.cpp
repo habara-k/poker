8b8fdc7f94f59f592129b5c6ef31066202d4d75b
keigo habara <34413567+habara-k@users.noreply.github.com>
2020-07-14 13:41:37 +0900

    fix log (#15)

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ using namespace poker;
 int main() {
     std::ofstream log("log.txt");
     std::streambuf* old = std::clog.rdbuf(log.rdbuf());
-    std::clog.rdbuf(old);
 
     Environment env({"check_fold", "user", "check_fold", "check_call", "check_fold", "check_call"});
     env.Run();
+
+    std::clog.rdbuf(old);
 
     return 0;
 }
